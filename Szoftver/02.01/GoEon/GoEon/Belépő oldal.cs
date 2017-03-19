61e76829b71d02f53e9fb33c78d37387e4b05eca
Udvardi.David <udvardi.dave@gmail.com>
2017-03-19 23:26:07 +0100

    GoEon 02.01

---
+++ Szoftver/02.01/GoEon/GoEon/Belépő oldal.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoEon
+{
+    public partial class Belépő_oldal : Form
+    {
+        Database_Manager db = new Database_Manager();
+        List<Feladatok> feladatok = new List<Feladatok>();
+        List<Team> csapatok = new List<Team>();
+        public Belépő_oldal()
+        {
+            InitializeComponent();
+            Gecko.Xpcom.Initialize(@"..\xulrunner");
+        }
+
+        private void Belépő_oldal_Load(object sender, EventArgs e)
+        {
+            feltolt();
+            csapatok = db.Read_Team(csapatok);
+            WebBrowser.Navigate("file:///F:/Programoz%C3%A1s/Szakdoga/GoEon/GoEon/bin/Debug/goeon_medium_icon.jpg");
+
+        }
+
+        public void feltolt()
+        {
+            feladatok.Clear();
+            db.Read_Task(feladatok);
+
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+
+            mai_hatarido_listBox.Items.Clear();
+
+            foreach (Feladatok it in feladatok)
+            {
+                if (it.getHatarido().Equals(today))
+                {
+                    mai_hatarido_listBox.Items.Add(it.getFeladat_neve());
+                    mai_hatarido_listBox.Items.Add("      Határidő: " + it.getHatarido());
+                    mai_hatarido_listBox.Items.Add("      Helyszín: " + it.getVaros() + ", " + it.getUtca() + ", " + it.getHazszam());
+                    mai_hatarido_listBox.Items.Add("      Prioritás: " + it.getPrioritas());
+                    mai_hatarido_listBox.Items.Add("\n");
+                }
+            }
+        }
+
+        //Ideiglenes, ha hozzáadtunk egy feladatot egy csapathoz akkor azok nem jelennek meg
+        public void feltolt(List<Feladatok> lista)
+        {
+            feladatok.Clear();
+            feladatok = lista;
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+
+            mai_hatarido_listBox.Items.Clear();
+
+            foreach (Feladatok it in feladatok)
+            {
+                if (it.getHatarido().Equals(today))
+                {
+                    mai_hatarido_listBox.Items.Add(it.getFeladat_neve());
+                    mai_hatarido_listBox.Items.Add("      Határidő: " + it.getHatarido());
+                    mai_hatarido_listBox.Items.Add("      Helyszín: " + it.getVaros() + ", " + it.getUtca() + ", " + it.getHazszam());
+                    mai_hatarido_listBox.Items.Add("      Prioritás: " + it.getPrioritas());
+                    mai_hatarido_listBox.Items.Add("\n");
+                }
+            }
+        }
+
+        private void tovabb_Button_Click(object sender, EventArgs e)
+        {
+            Fooldal fooldal = new Fooldal(this);
+            fooldal.Show();            
+        }
+
+        private void csapat_hozzarendeles_Button_Click(object sender, EventArgs e)
+        {
+            Csapat_hozzarendeles uj = new Csapat_hozzarendeles(feladatok, csapatok, "Mai");
+            uj.Show();
+        }
+
+        private void mai_hatarido_listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(mai_hatarido_listBox.SelectedItem != null)
+            {
+                foreach(Feladatok it in feladatok)
+                {
+                    if (mai_hatarido_listBox.SelectedItem.Equals(it.getFeladat_neve()))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("www.google.com/maps/place/");
+                        sb.Append(it.getVaros() + ",+" + it.getUtca() + "+" + it.getHazszam());
+                        WebBrowser.Navigate(sb.ToString());
+                    }
+                }
+            }
+        }
+
+        private void összesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mai_hatarido_listBox.Items.Clear();
+            foreach (Feladatok it in feladatok)
+            {
+                mai_hatarido_listBox.Items.Add(it.getFeladat_neve());
+                mai_hatarido_listBox.Items.Add("\tHatáridő: " + it.getHatarido());
+                mai_hatarido_listBox.Items.Add("\tHelyszín: " + it.getVaros() + ", " + it.getUtca() + ", " + it.getHazszam());
+                mai_hatarido_listBox.Items.Add("\tPrioritás:" + it.getPrioritas());
+                mai_hatarido_listBox.Items.Add("\n");
+            }
+        }
+
+        private void maiHatáridővelRendelkezőFeladatokToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mai_hatarido_listBox.Items.Clear();
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+
+            foreach (Feladatok it in feladatok)
+            {
+                if (it.getHatarido().Equals(today))
+                {
+                    mai_hatarido_listBox.Items.Add(it.getFeladat_neve());
+                    mai_hatarido_listBox.Items.Add("\tHatáridő: " + it.getHatarido());
+                    mai_hatarido_listBox.Items.Add("\tHelyszín: " + it.getVaros() + ", " + it.getUtca() + ", " + it.getHazszam());
+                    mai_hatarido_listBox.Items.Add("\tPrioritás:" + it.getPrioritas());
+                    mai_hatarido_listBox.Items.Add("\n");
+                }
+            }
+        }
+
+        private void sürgősFeladatokToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mai_hatarido_listBox.Items.Clear();
+            foreach (Feladatok it in feladatok)
+            {
+                if (it.getPrioritas().Equals("Sürgős"))
+                {
+                    mai_hatarido_listBox.Items.Add(it.getFeladat_neve());
+                    mai_hatarido_listBox.Items.Add("\tHatáridő: " + it.getHatarido());
+                    mai_hatarido_listBox.Items.Add("\tHelyszín: " + it.getVaros() + ", " + it.getUtca() + ", " + it.getHazszam());
+                    mai_hatarido_listBox.Items.Add("\tPrioritás:" + it.getPrioritas());
+                    mai_hatarido_listBox.Items.Add("\n");
+                }
+            }
+        }
+
+        private void normálFeladatokToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mai_hatarido_listBox.Items.Clear();
+            foreach (Feladatok it in feladatok)
+            {
+                if (it.getPrioritas().Equals("Normál"))
+                {
+                    mai_hatarido_listBox.Items.Add(it.getFeladat_neve());
+                    mai_hatarido_listBox.Items.Add("\tHatáridő: " + it.getHatarido());
+                    mai_hatarido_listBox.Items.Add("\tHelyszín: " + it.getVaros() + ", " + it.getUtca() + ", " + it.getHazszam());
+                    mai_hatarido_listBox.Items.Add("\tPrioritás:" + it.getPrioritas());
+                    mai_hatarido_listBox.Items.Add("\n");
+                }
+            }
+        }
+    }
+}
---
+++ Szoftver/02.01/GoEon/GoEon/Classes/Task.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoEon
+{
+    public class Feladatok
+    {
+        protected string feladat_neve;
+        protected string varos;
+        protected string utca;
+        protected int hazszam;
+        protected string leiras;
+        protected string hatarido;       //2016-11-04 formátum
+        protected string rogzites_ideje; //2016-11-04 formátum
+        protected string prioritas;
+        string eszkoz;
+        string kepesites;
+        public List<string> eszkozok = new List<string>();
+        public List<string> kepesitesek = new List<string>();
+        protected string megjegyzes;
+
+        public List<string> Eszkozok
+        {
+            get
+            {
+                return eszkozok;
+            }
+
+            set
+            {
+                eszkozok = value;
+            }
+        }
+
+        public Feladatok()
+        {
+            feladat_neve = "";
+            varos = "";
+            utca = "";
+            hazszam = 0;
+            leiras = "";
+            hatarido = "";
+            rogzites_ideje = "";
+            prioritas = "";
+            megjegyzes = "";
+
+        }
+
+        public Feladatok(string fnev, string varos, string utca, int hazszam,
+            string leiras, string hatarido, string rogzites_ideje, string prioritas,
+            string eszkoz, string kepesites, string megjegyzes)
+        {
+            this.feladat_neve = fnev;
+            this.varos = varos;
+            this.utca = utca;
+            this.hazszam = hazszam;
+            this.leiras = leiras;
+            this.hatarido = hatarido;
+            this.rogzites_ideje = rogzites_ideje;
+            this.prioritas = prioritas;
+            this.eszkoz = eszkoz;
+
+            string[] str = eszkoz.Split(',');
+            foreach (string i in str)
+            {
+                if (!i.Equals(""))
+                {
+                    eszkozok.Add(i);
+                }
+            }
+
+            this.kepesites = kepesites;
+            string[] str2 = kepesites.Split(',');
+            foreach (string i in str2)
+            {
+                if (!i.Equals(""))
+                {
+                    kepesitesek.Add(i);
+                }
+            }
+
+
+            this.megjegyzes = megjegyzes;
+
+        }
+
+
+        public string getFeladat_neve()
+        {
+            return feladat_neve;
+        }
+
+        public string getVaros()
+        {
+            return varos;
+        }
+
+        public string getUtca()
+        {
+            return utca;
+        }
+
+        public int getHazszam()
+        {
+            return hazszam;
+        }
+
+        public string getLeiras()
+        {
+            return leiras;
+        }
+
+        public string getHatarido()
+        {
+            return hatarido;
+        }
+
+        public string getRogzites_ideje()
+        {
+            return rogzites_ideje;
+        }
+
+        public string getPrioritas()
+        {
+            return prioritas;
+        }
+
+        public string getMegjegyzes()
+        {
+            return megjegyzes;
+        }
+ 
+    }
+}
